d6f7ded2eeec78e53990ba6be9867308eba5bc4e
Shaban Irshad <shabanirshad408@gmail.com>
2026-03-05 11:33:00 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
-
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
